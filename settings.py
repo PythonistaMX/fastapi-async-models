130b4b3dc7c716c3bf8a96abe0b07bc0991bd9ec
Jose Luis Chiquete <josech@gmail.com>
2022-08-12 20:30:43 -0500

    Modificación a requirements.txt

--- settings.py
+++ settings.py
@@ -5,11 +5,10 @@ ENVIRONMENT = getenv('ENV')
 DB_USER = getenv('DB_USER')
 DB_PASSWORD = getenv('DB_PASSWORD')
 
-if  getenv('GAE_ENV') == 'standard':
+if getenv('GAE_ENV') == 'standard':
     SQLALCHEMY_DATABASE_URL = 'sqlite+aiosqlite:///db.sqlite3'
 else:
     SQLALCHEMY_DATABASE_URL = 'sqlite+aiosqlite:///db.sqlite3'
     '''
     SQLALCHEMY_DATABASE_URL = f'mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@localhost/pythonista
     '''
-    
