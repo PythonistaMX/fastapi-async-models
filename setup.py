57513241c72db13af12dccb27772740ba78b8610
Jose Luis Chiquete <josech@gmail.com>
2022-08-22 18:36:51 -0500

    220822a

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fastapi_async_models",
+    version="0.1.0",
+    description="Paquete que incluye una aplicación API REST hecha con FastAPI.",
+    packages=find_packages(),
+    package_data={'readme':['README.md']}
+)
